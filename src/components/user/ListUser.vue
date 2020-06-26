ac558dd21c369cdda3b3ea7740faf3db2bf3992e
Pradeepta Khatoi <pradeepta.khatoi@citiustech.com>
2020-06-26 19:22:12 +0530

    Implemented delete user functionality.

--- src/components/user/ListUser.vue
+++ src/components/user/ListUser.vue
@@ -11,7 +11,7 @@
           <th scope="col">Actions</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody v-if="(users && users.length > 0)">
         <tr v-for="user in users" v-bind:key="user.id"> 
           <th scope="row">{{user.id}}</th>
           <td>{{user.name}}</td>
@@ -20,10 +20,11 @@
           <td>
             <router-link v-bind:to="{ name: 'ViewUser', params: {id: user.id } }" class="btn btn-primary btn-sm">View</router-link>&nbsp;&nbsp; 
             <router-link v-bind:to="{ name: 'EditUser', params: {id: user.id } }" class="btn btn-secondary btn-sm">Edit</router-link>&nbsp;&nbsp;
-            <router-link v-bind:to="{ name: 'ViewUser', params: {id: user.id } }" class="btn btn-danger btn-sm">Delete</router-link>
+            <a href="javascript:;" @click="deleteUser(user)" class="btn btn-danger btn-sm">Delete</a>
           </td>
         </tr>
       </tbody>
+      <tbody v-else><tr><td colspan="5">No User Found!!</td></tr></tbody>
     </table> 
   </div> 
 </template>
@@ -34,10 +35,10 @@ import axios from 'axios';
 export default {
   name: 'UserList',
    data: function() {
-        return {
-            heading: "User List",
-            users: null,
-        }
+      return {
+        heading: "User List",
+        users: [],
+      }
     },
     created: function() {
       axios
@@ -46,6 +47,13 @@ export default {
         .then(res => {
           this.users = res;
         })
+    },
+    methods: {
+    deleteUser : function(user) {
+        if(confirm('Are you sure you want to delete this user?')) {
+          this.users = this.users.filter((u) => u.id != user.id);
+        }        
+      }
     }
 }
 </script>
