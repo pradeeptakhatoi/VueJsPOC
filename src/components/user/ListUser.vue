60b5301d1d681c192e9f74dca80940ec2dee6551
Pradeepta Khatoi <pradeepta.khatoi@citiustech.com>
2020-06-26 17:56:24 +0530

    Added axios library for ajax call & implemented user listing.

--- src/components/user/ListUser.vue
+++ src/components/user/ListUser.vue
@@ -1,16 +1,50 @@
 <template>
-  <div class="hello">
-    <h1>{{ heading }}</h1>    
-  </div>
+   <div class="container">
+    <h3> Users:</h3>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">City</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" v-bind:key="user.id"> 
+          <th scope="row">{{user.id}}</th>
+          <td>{{user.name}}</td>
+          <td>{{user.email}}</td>
+          <td>{{user.address.city}}</td>
+          <td>
+            <a href="javascript:;">View</a> | 
+            <a href="javascript:;">Edit</a> | 
+            <a href="javascript:;">Delete</a>
+          </td>
+        </tr>
+      </tbody>
+    </table> 
+  </div> 
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'UserList',
    data: function() {
         return {
-            heading: "User List"
+            heading: "User List",
+            users: null,
         }
     },
+    created: function() {
+      axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+          this.users = res.data;
+        })
+    }
 }
 </script>
